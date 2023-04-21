177a3a2913bc552301ab24f535065f00f52e1547
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-21 14:04:50 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PopularNews, LatestNews, Home } from './pages/pages';
-import { HistoryRouter as Router } from 'redux-first-history/rr6';
-import { Routes, Route } from 'react-router-dom';
-import store from './redux/store';
-import { history } from './redux/store';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './components/App';
+
+ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Router history={history}>
-                <App>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/latest-news" element={<LatestNews />} />
-                        <Route path="/popular-news" element={<PopularNews />} />
-                    </Routes>
-                </App>
-            </Router>
-        </Provider>
-    </React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
--- src/pages/pages.js
+++
@@ -1,5 +0,0 @@
-import Home from '../pages/Home/Home';
-import PopularNews from './PopularNews/PopularNews';
-import LatestNews from './LatestNews/LatestNews';
-
-export { Home, PopularNews, LatestNews };
