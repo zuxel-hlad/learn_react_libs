bc38151b569af93c3f1f2420b57399a1187e9093
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-05-11 11:59:40 +0300

    completed lesson about tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import styles from './App.module.scss';
+import { useState } from 'react';
+
+const App = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className={styles.parent}>
+            <h1 className={styles.heading}>Helo Tailwind</h1>
+            <div>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button
+                    className={styles.button}
+                    onClick={() => setIsOpen(prevOpen => (prevOpen = !isOpen))}
+                >
+                    Login
+                </button>
+            </div>
+            {/* trasitions example*/}
+            {isOpen && <button className="animate-fade">TEST</button>}
+            <div className={styles.wrapper}>
+                <span>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Praesentium, itaque?
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#61dafb',
-        },
-        secondary: {
-            main: '#ff0090',
-        },
-    },
-});
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
